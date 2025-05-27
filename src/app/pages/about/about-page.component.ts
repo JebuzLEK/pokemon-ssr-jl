df620d4b9d74ee381cb516456407e9b99ae8204f
jesus <jesus.lazob1@gmail.com>
2025-05-26 20:05:39 -0400

    before ssr changes

---
+++ src/app/pages/about/about-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css'
+})
+export default class AboutPageComponent {
+
+}
